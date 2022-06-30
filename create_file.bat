7a838c6bf456a38a5329f88f906e5d39837eb4d0
GuiNobre1602 <guinobre1602@gmail.com>
2022-06-30 18:45:32 -0300

    correcao de bugs e adição de escolha arquivo

--- create_file.bat
+++ create_file.bat
@@ -21,24 +21,25 @@ cls
 echo: 
 echo Insira o nome do seu arquivo .l (ele deve estar na mesma pasta): 
 set /p name=
-echo Your name is %filename%
 
-echo:"CRIANDO O ARQUIVO lex.yy.c"
+echo:CRIANDO O ARQUIVO lex.yy.c a partir de %filename%
 flex %name%
 echo:
 
 echo Qual nome do arquivo compilador deseja: 
 set /p compilername=
-echo Your name is %compilername%
+echo:
 
-echo:"Compilando o testador chamado" %compilername%
-pause
+echo:Compilando o testador chamado %compilername%
+echo:
 gcc lex.yy.c -o %compilername% -lfl
 echo:
 
-echo:"Fazendo a analise lexica"
-pause
-%compilername% < dados.txt
+echo:Fazer analise lexa de qual arquivo? (preferencia .txt)
+set /p filetoscan=
+echo:Fazer analise lexa de qual arquivo? (preferencia .txt)
+echo:
+%compilername% < %filetoscan%
 echo:
 
 pause
